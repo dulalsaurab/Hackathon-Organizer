6f909b423c2396b2c339bdbdd941c33a60a62094
Alejandro Gil Torres <antrek135@gmail.com>
2018-04-06 10:36:36 -0500

    Log in and signUp HTML created. Validations for username and email. routes for user. user_controles updated, but not finished.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+    validates :user_name, presence: true, length: { maximum: 50 }, uniqueness: {case_sensitive: false}
+    before_save { self.email = email.downcase }
+    EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 150 }, format: {with: EMAIL}, uniqueness: {case_sensitive: false}
+    #CHECK FOR QUICK SUBMISSIONS ADDING AN INDEX TO USEREMAIL. 6.29
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  #get 'sessions/new'
+
   root :to => "homepage#home"
+  #TODO
+  get    '/signup',  to: 'users#new'
   
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   get 'homepage/home'
 
   get 'homepage/new'

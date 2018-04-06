07a8f28d0df92ec68279cba9516007fb240352f4
Alejandro Gil Torres <antrek135@gmail.com>
2018-04-06 10:56:54 -0500

    added validation for the password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,12 +10,12 @@ class UserTest < ActiveSupport::TestCase
   end
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
-    assert @user.valid?
+    assert_not @user.valid?
   end
 
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
-    assert @user.valid?
+    assert_not @user.valid?
   end
 
 end

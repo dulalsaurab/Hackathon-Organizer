522b1cf4d6e24fc50f9e434c36872ef360371fa0
Alejandro Gil Torres <antrek135@gmail.com>
2018-04-11 01:26:35 -0500

    LAST COMMIT WORKING WITH SEED.RB WORKING

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,21 +21,7 @@
 
 
 =begin
-User.create!(user_name:  "Admin",
-             email: "admin@admin.com",
-             password:              "password",
-             password_confirmation: "password",
-             admin: true)
 
-10.times do |n|
-    name = Faker::Name.name
-    email = "example-#{n+1}@hkorg.com"
-    password = 'password'
-    User.create!(user_name: name,
-                 email: email,
-                 password: password,
-                 password_confirmation: password)
-end
 
 proposal_1= Proposal.new(userId: 1, title: 'first proposal', hkId: 1, description: 'hello world', customForm: 'form', votes: 0)
 proposal_2= Proposal.new(userId: 2, title: '2nd proposal', hkId: 2, description: 'hello world', customForm: 'form', votes: 0)
@@ -53,10 +39,26 @@ hackathon_1.save!
 hackathon_2.save!
 =end
 
-user_1 = User.new(user_name: 'Alex', email: 'aaaa@aaa.com', passworld: 'topsecret' )
-user_2 = User.new(user_name: 'Saurab', email: 'aaaa@aaa.com', passworld: 'topsecret' )
-user_3 = User.new(user_name: 'Sajib', email: 'aaaa@aaa.com', passworld: 'topsecret' )
-admin = User.new(user_name: 'Admin', email: 'aaaa@aaa.com', passworld: 'topsecretadmin', admin: true )
+#user_1 = User.new(user_name: 'Alex', email: 'aaaa@aaa.com', passworld: 'topsecret' )
+#user_2 = User.new(user_name: 'Saurab', email: 'aaaa@aaa.com', passworld: 'topsecret' )
+#user_3 = User.new(user_name: 'Sajib', email: 'aaaa@aaa.com', passworld: 'topsecret' )
+#admin = User.new(user_name: 'Admin', email: 'aaaa@aaa.com', passworld: 'topsecretadmin', admin: true )
+
+User.create!(user_name:  "Admin",
+             email: "admin@admin.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
+
+10.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n+1}@hkorg.com"
+    password = 'password'
+    User.create!(user_name: name,
+                 email: email,
+                 password: password,
+                 password_confirmation: password)
+end
 hackathon_1 = Hackathon.new(title: 'First one ever', topic: 'fantasy', description: 'The coolest', owner: 1, start_date: '2012/03/11', end_date: '2012/03/11', hackathon_venue: "west minister")
 
 hackathon_2 = Hackathon.new(title: 'Second one ever', topic: 'Tech', description: 'The hotest', owner: 1, start_date: '2012/03/11', end_date: '2012/03/11', hackathon_venue: "east minister")
@@ -92,10 +94,7 @@ hackathon_1.user= user_3
 hackathon_1.user= admin
 
 
-user_1.save!
-user_2.save!
-user_3.save!
-admin.save!
+
 hackathon_1.save!
 hackathon_2.save!
 

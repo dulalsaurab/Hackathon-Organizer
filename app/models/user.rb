9da10c93450c23b1497b1430cc5bf8b282f40e80
dulalsaurab <dulal.saurab@gmail.com>
2018-04-11 06:05:55 +0000

    fixed merged conflit and user creation problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :hackathons
     has_many :proposals
-=begin
+
     attr_accessor :remember_token
     before_save { self.email = email.downcase }
     validates :user_name, presence: true, length: { maximum: 50 }, uniqueness: {case_sensitive: false}
@@ -35,5 +35,5 @@ class User < ApplicationRecord
     def forget
         update_attribute(:remember_me, nil)
     end
-=end
+
 end

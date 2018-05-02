c3b57c12bd79eddcce5be02447bc418c736024bb
Saurab Dulal <dulal.saurab@gmail.com>
2018-05-02 20:47:03 +0000

    some changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502120333) do
+ActiveRecord::Schema.define(version: 20180502143055) do
 
   create_table "custome_tables", force: :cascade do |t|
     t.string "question"
